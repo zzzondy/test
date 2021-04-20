eac8b3baec7c7b4f868aa5a0f932e85eca2f41d3
Artyom Rodionov <72665171+zzzondy@users.noreply.github.com>
2021-04-20 15:45:28 +0300

    сглыпа

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+import random
+
+app = Flask(__name__)
+
+
+@app.route('/index')
+def index():
+    return 'Ты лох'
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
