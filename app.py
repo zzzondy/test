87023cdb18dcc731d970d4ade61b80a6b8434318
Artyom Rodionov <72665171+zzzondy@users.noreply.github.com>
2021-04-21 16:08:11 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 from flask import Flask
 import random
-import os
 
 app = Flask(__name__)
-
 
 @app.route('/')
 @app.route('/index')
@@ -12,5 +10,4 @@ def index():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
